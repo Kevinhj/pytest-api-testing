cea87c9baee5aaa4a8735e374f5a0d0c34a88ae6
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-05-11 17:07:55 -0600

    fix a typo

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # pytest-api-testing
 
 To run all tests
+
 `pytest`
+
+To run test cases by mark
+
+`pytest -m tcid29`
+
+To run the env file on Windows
+
+`env.bat`
--- wooapitest/tests/customer/test_create_customers_smoke.py
+++ wooapitest/tests/customer/test_create_customers_smoke.py
@@ -6,7 +6,7 @@ from wooapitest.src.dao.customers_dao import CustomersDAO
 
 @pytest.mark.tcid29
 def test_create_customer_only_email_password():
-    logger.info("TEST: Ceate new customer with email and password only")
+    logger.info("TEST: Create new customer with email and password only")
 
     rand_info = generate_random_email_and_password()
     logger.info(rand_info)
