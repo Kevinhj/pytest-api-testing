fc73b062013aa147acee297d3ef42b8e39f58417
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-05-11 22:31:02 -0600

    Add customer negative test and reporting

--- wooapitest/tests/customer/test_create_customers_smoke.py
+++ wooapitest/tests/customer/test_create_customers_smoke.py
@@ -3,7 +3,9 @@ import logging as logger
 from wooapitest.src.utilities.genericUtilities import generate_random_email_and_password
 from wooapitest.src.helpers.customers_helper import CustomerHelper
 from wooapitest.src.dao.customers_dao import CustomersDAO
+from wooapitest.src.utilities.requestsUtility import RequestUtility
 
+@pytest.mark.customers
 @pytest.mark.tcid29
 def test_create_customer_only_email_password():
     logger.info("TEST: Create new customer with email and password only")
@@ -37,6 +39,7 @@ def test_create_customer_only_email_password():
     assert id_in_api == id_in_db, f'Create customer response "id" not same as "ID" in database.' \
                                   f'Email: {email}'
 
+@pytest.mark.customers
 @pytest.mark.tcid47
 def test_create_customer_fail_for_existing_email():
 
@@ -45,8 +48,20 @@ def test_create_customer_fail_for_existing_email():
     existing_cust = cust_dao.get_random_customer_from_db()
     existing_email = existing_cust[0]['user_email']
 
-    # call the api
-    cust_obj = CustomerHelper()
-    cust_api_info = cust_obj.create_customer(email=existing_email, password="Password1")
+    # create the request object
+    req_helper = RequestUtility()
 
-    import pdb; pdb.set_trace()
+    # create the payload with existing email, then use the post method on request helper to attempt to create the
+    # customer the request response is a 400, and then I validate the error message in the response
+    payload = {"email": existing_email, "password": "Password1"}
+    cust_api_info = req_helper.post(endpoint='customers', payload=payload, expected_status_code=400)
+
+    assert cust_api_info['code'] == 'registration-error-email-exists', f"Create customer with " \
+        f"existing user error 'code' is not correct. Expected: 'registration-error-email-exists', " \
+        f"Actual: {cust_api_info['code']}"
+
+    assert cust_api_info['message'] == 'An account is already registered with your email address. <a href="#" ' \
+        'class="showlogin">Please log in.</a>', \
+        f"Create customer with existing user error 'message' is not correct. " \
+        'An account is already registered with your email address. <a href="#" class="showlogin">Please log in.</a>' \
+        f"Actual: {cust_api_info['message']}"
